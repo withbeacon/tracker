b9329cf343a1a68f1935c34f11f7c1b7d5033dba
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-02-22 21:14:23 +0530

    revert(core): use tsup to build

---
+++ packages/core/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  treeshake: false,
+  dts: true,
+  format: ["esm", "cjs"],
+  outDir: "dist", 
+  entry: ["./src/index.ts"],
+});
