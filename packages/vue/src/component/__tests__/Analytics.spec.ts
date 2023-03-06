b96ac139e38a1710ff7f2b18440de4f66735cd6f
selemondev <selemondev19@gmail.com>
2023-03-06 22:28:49 +0300

    feat: add support for vue 3

--- packages/vue/src/component/__tests__/Analytics.spec.ts
+++
@@ -1,7 +0,0 @@
-import Analytics from '../Analytics.vue'
-import { describe, it, expect } from 'vitest'
-describe('Analytics Component', () => {
-    it('Exists', async () => {
-        expect(Analytics).exist
-    })
-})
